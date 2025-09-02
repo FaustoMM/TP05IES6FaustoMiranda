00838d3a47b306be835ce532ba6569322594059b
FaustoMM <faustosanti01@gmail.com>
2025-09-02 11:59:26 -0300

    Inicializacion del TP05 IES6 Fausto Miranda

---
+++ src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //java es fuertemente tipado
+        //void ejecuta pero no devuelve nada
+        System.out.println("Hello, World!");
+
+        //Definicion e inicializacion
+        //datos primitivos
+        int numeroA = 10;
+        int numeroB = 5;
+        double numeroD = 4.2;
+        boolean bandera = true;
+        char caracter = 'C';
+
+        
+        //DATOS TIPO OBJETOS
+        
+
+
+        numeroA = 30;
+
+        System.out.println("La suma de dos numero es " + (numeroA+numeroB));
+    }
+}
